58eda68bb30b000e7b31956aa204ace335bea181
Brian Sorahan <bsorahan@gmail.com>
2013-03-11 17:26:56 -0500

    honing the makefile

--- libraries/snd/src/snd/Snd.java
+++ libraries/snd/src/snd/Snd.java
@@ -3,5 +3,18 @@ package snd;
 import processing.core.*;
 
 public class Snd {
+    private PApplet papplet;
+    private static Snd instance;
 
+    private Snd() {}
+
+    private Snd(PApplet papplet) {
+	this.papplet = papplet;
+    }
+
+    public static Snd getInstance(PApplet papplet) {
+	if (instance == null)
+	    instance = new Snd(papplet);
+	return instance;
+    }
 }
